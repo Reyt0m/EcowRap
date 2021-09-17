351eafbb81ae864b2150b37356b793b4661d3d50
reytom <reytom1715@gmail.com>
2021-09-17 23:57:05 +0900

    hui 削除

--- src/qrcode.js
+++ src/qrcode.js
@@ -1,12 +1,4 @@
-const showMessage = () => {
-    const textbox = document.getElementById("input-message");
-    const inputValue = textbox.value;
 
-    //テキストボックスの値を使って、出力するメッセージを生成する
-    const output = "入力された内容は「" + inputValue + "」です。";
-    //出力用のp要素にメッセージを表示
-    document.getElementById("output-message").innerHTML = output;
-    }
 
 const qrDiv = document.querySelectorAll( '[data-url]' );
 new QRCode( qrDiv, {
