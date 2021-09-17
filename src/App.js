d5aa582f7b1bfa240042f87e23997d6cb80d117e
reytom <reytom1715@gmail.com>
2021-09-17 13:46:21 +0900

    website

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
-const showMessage = () => {
-    const textbox = document.getElementById("input-message");
-    const inputValue = textbox.value;
-
-    //テキストボックスの値を使って、出力するメッセージを生成する
-    const output = "入力された内容は「" + inputValue + "」です。";
-    //出力用のp要素にメッセージを表示
-    document.getElementById("output-message").innerHTML = output;
-    makeMessage()
-    }
-
-const makeMessage = () => {
-    ARtext = document.getElementsByTagName("a-box").setAttribute("color",inputValue);
-    console.log(ARtext)
+//1 showvalue 更新
+function buttonClick(event) {
+  showValueEl.setAttribute("value", messageArea.value);
+  //   デバッグ
+  
+  console.log(messageArea.value);
+  console.log(showValueEl.getAttribute("value"));
 }
-// AFRAME.registerComponent('plane', {
-//     init: function () {
-//       this.canvas = document.getElementById('canvas');
-//       this.ctx = canvas.getContext('2d');
-//     },
-//     tick: function () {
-//       const material = this.el.getObject3D('mesh').material;
-//       const fontSize = 24;
 
-//       this.canvas.width = this.canvas.width;
-//       this.canvas.height = this.canvas.height;
+//   取得
+const messageArea = document.getElementById("messageArea");
+//   a-text 取得
+const showValueEl = document.querySelector("#textMessages");
+// a-text value 取得
+//   inputvalue変更読み取り
+const checkButton = document.getElementById('checkButton')
+checkButton.addEventListener('click', buttonClick);
 
-//       this.ctx.save();
-//         this.ctx.font = `${fontSize}px sans-serif`;
-//         this.ctx.fillText(Date.now(), 0, fontSize);
-//       this.ctx.restore();
-
-//       if (!material.map) {
-//         return;
-//       }
-
-//       material.map.needsUpdate = true;
-//     }
-//   });
-
-//   const plane = document.createElement('a-plane');
-
-//   plane.setAttribute('plane', 'plane');
-//   plane.setAttribute('material', 'shader: flat; src: #canvas;');
-
-//   document.getElementsByTagName('a-scene')[0].appendChild(plane);
